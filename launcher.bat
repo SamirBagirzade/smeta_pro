e2c412e00d6d50230cbac41e21bb168373ad4cfd
SamirBagirzade <samir.bagirzade2@gmail.com>
2026-01-16 12:32:11 +0400

    Fixed launcher

--- launcher.bat
+++ launcher.bat
@@ -11,44 +11,34 @@ set "VENV_DIR=.venv"
 set "REQ_FILE=requirements.txt"
 REM ====================
 
-where git >nul 2>&1 || (echo ERROR: git not found in PATH & timeout /t 5 & exit /b 1)
-where python >nul 2>&1 || (echo ERROR: python not found in PATH & timeout /t 5 & exit /b 1)
+where git >nul 2>&1 || (exit /b 1)
+where python >nul 2>&1 || (exit /b 1)
 
 if not exist "%REPO_DIR%\" (
-  echo Repo not found. Cloning...
-  git clone "%REPO_URL%" "%REPO_DIR%" || (timeout /t 5 & exit /b 1)
+  git clone "%REPO_URL%" "%REPO_DIR%" >nul 2>&1 || exit /b 1
 ) else (
-  echo Repo exists. Pulling latest...
-  pushd "%REPO_DIR%" || (timeout /t 5 & exit /b 1)
-  git fetch --all || (popd & timeout /t 5 & exit /b 1)
+  pushd "%REPO_DIR%" || exit /b 1
+  git fetch --all >nul 2>&1 || (popd & exit /b 1)
   if not "%BRANCH%"=="" (
-    git checkout "%BRANCH%" || (popd & timeout /t 5 & exit /b 1)
+    git checkout "%BRANCH%" >nul 2>&1 || (popd & exit /b 1)
   )
-  git pull || (popd & timeout /t 5 & exit /b 1)
+  git pull >nul 2>&1 || (popd & exit /b 1)
   popd
 )
 
-pushd "%REPO_DIR%" || (timeout /t 5 & exit /b 1)
+pushd "%REPO_DIR%" || exit /b 1
 
 if "%USE_VENV%"=="1" (
   if not exist "%VENV_DIR%\Scripts\python.exe" (
-    echo Creating venv...
-    python -m venv "%VENV_DIR%" || (popd & timeout /t 5 & exit /b 1)
+    python -m venv "%VENV_DIR%" >nul 2>&1 || (popd & exit /b 1)
   )
 
   if exist "%REQ_FILE%" (
-    echo Installing/updating requirements...
-    "%VENV_DIR%\Scripts\python.exe" -m pip install --upgrade pip >nul 2>&1 || (popd & timeout /t 5 & exit /b 1)
-    "%VENV_DIR%\Scripts\python.exe" -m pip install -r "%REQ_FILE%" >nul 2>&1 || (popd & timeout /t 5 & exit /b 1)
+    "%VENV_DIR%\Scripts\python.exe" -m pip install --upgrade pip >nul 2>&1
+    "%VENV_DIR%\Scripts\python.exe" -m pip install -r "%REQ_FILE%" >nul 2>&1
   )
-
-  echo Starting program...
-  start "" /B "%VENV_DIR%\Scripts\pythonw.exe" "%PY_ENTRY%" >nul 2>&1
-) else (
-  echo Starting program...
-  start "" /B pythonw.exe "%PY_ENTRY%" >nul 2>&1
 )
 
+REM Don't launch Python here - VBS will do it
 popd
 endlocal
-exit
